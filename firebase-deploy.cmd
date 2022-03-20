2566b12fe0aa38528bd45fc42b68d28c8e9465c7
6zs <49460548+6zs@users.noreply.github.com>
2022-03-20 15:30:01 -0700

    remove ignored files

--- firebase-deploy.cmd
+++
@@ -1,4 +0,0 @@
-npm run build
-xcopy build firebase-build /E/H/y
-powershell -Command "(gc firebase-build/index.html) -replace '/xordle/', '/' | Out-File -encoding ASCII firebase-build/index.html"
-firebase deploy
